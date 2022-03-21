6e01816303a873ada83f157fcd0e17daa84f01af
svenmaerki <sven.maerki@gmx.ch>
2022-03-21 16:32:01 +0100

    added js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function copyText() {
+    var copiedText;
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    alert("Copied the text: " + copyText.value);
+  }
